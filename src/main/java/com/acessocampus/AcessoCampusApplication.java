79431644075a98dc3b656bece298312c3c6b0e95
mateus-ceccagno <sk.mateus@hotmail.com>
2022-12-01 22:23:18 -0300

    add mysql db

--- src/main/java/com/acessocampus/AcessoCampusApplication.java
+++ src/main/java/com/acessocampus/AcessoCampusApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AcessoCampusApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AcessoCampusApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+            SpringApplication.run(AcessoCampusApplication.class, args);
+    }
 }
